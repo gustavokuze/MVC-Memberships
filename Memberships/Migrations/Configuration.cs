7b49308c9d4946bc57be872cca3c16831598eb5b
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2018-11-18 11:33:57 -0300

    Added connection string

---
+++ Memberships/Migrations/Configuration.cs
@@ -0,0 +1,30 @@
+namespace Memberships.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Memberships.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+
+            AutomaticMigrationsEnabled = true;
+
+            /*
+             Isso aqui embaixo diz que permitimos perda de dados (!?) 
+             
+             */
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(Memberships.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
